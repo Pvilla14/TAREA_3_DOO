36d4cad2f6edae406ef486f249d3b6f7a95d812e
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2024-05-28 23:52:52 -0400

    Se me olvido este archivo en el commit anterior xd

--- src/main/java/Visual/MainVisual.java
+++ src/main/java/Visual/MainVisual.java
@@ -1,6 +1,7 @@
 package Visual;
 
 import Tarea.Producto;
+import Tarea.Valoresestaticos;
 import Tarea.clasemoneda.Moneda;
 
 import java.lang.reflect.Array;
@@ -8,22 +9,22 @@ import java.util.ArrayList;
 
 public class MainVisual {
     private ArrayList<Moneda> billetera = new ArrayList<Moneda>();
-    private Producto producto = null;
+    private Valoresestaticos producto = null;
     public static void main(String[] args) {
         MainVisual instanciamain = new MainVisual();
         Pantalla pantalla = new Pantalla(instanciamain);
     }
 
-    public void SeleccionarProducto(Producto p) {
+    public void SeleccionarProducto(Valoresestaticos p) {
         producto = p;
-        System.out.println("Producto: " + producto + "serie " + producto.getSerie());
+        System.out.println("Producto: " + producto + "serie " + producto);
 
     }
     public void cargarMoneda(Moneda m){
         billetera.add(m);
         System.out.println("Moneda: " + m + "Valor" +m.getValor());
     }
-    public Producto getCompra() {
+    public Valoresestaticos getCompra() {
         return producto;
     }
 }
