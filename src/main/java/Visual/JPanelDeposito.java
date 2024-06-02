20f80b6c94fef4b27b52bbf6b029a673bd6deef0
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2024-06-02 00:24:13 -0400

    Se hace uso del metodo getDeposito, y getTipo para hacer la impresion de los depositos en el Frame

--- src/main/java/Visual/JPanelDeposito.java
+++ src/main/java/Visual/JPanelDeposito.java
@@ -1,12 +1,15 @@
 package Visual;
 
 import Tarea.*;
+import Tarea.clasebebida.*;
+import Tarea.clasedulces.*;
+import Tarea.clasemoneda.*;
 import Visual.Botones.Boton100;
 import javax.swing.*;
 import java.awt.*;
 
 public class JPanelDeposito extends JPanel {
-    private Deposito<Producto> deposito = new Deposito<Producto>();
+    private Deposito<Producto> deposito;
     public JPanelDeposito(Deposito<Producto> depo){
         super();
         deposito = depo;
@@ -15,13 +18,52 @@ public class JPanelDeposito extends JPanel {
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-
-        ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Moneda.png"));
-        foto.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
-        //g.drawImage(foto.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH), 0, 0, this);
-        //Image foto2 = foto.getImage();
-        //g.drawImage(foto.getImage(), 0,0,this);
-        g.drawImage(foto.getImage(), 0,0,200,200, Color.black,this);
-
+        Object depositado = deposito.getTipo();
+        Object aux;
+        if (depositado.getClass() == (aux = new Fanta(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Fanta.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new CocaCola(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/CocaCola.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new Sprite(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Sprite.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new Serranita(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Serranita.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new Super8(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Super8.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new Snickers(-1)).getClass()) {
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Snickers.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else if(depositado.getClass() == (aux = new Moneda100(-0).getClass())){
+            for(int i = 0; i < deposito.getTamaño(); i++){
+                ImageIcon foto = new ImageIcon(Boton100.class.getResource("/Moneda100.png"));
+                g.drawImage(foto.getImage(), 20*i,0,50,50, Color.black,this);
+            }
+        }
+        else{
+            setBackground(Color.WHITE);
+        }
     }
 }
